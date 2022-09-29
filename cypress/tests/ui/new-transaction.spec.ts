0f3f5cd927f2a93ab7c740428d83e442c0cef3a7
Nick Reiter <nick@suitable.co>
2022-09-29 14:39:07 -0400

    initial code commit

---
+++ cypress/tests/ui/new-transaction.spec.ts
@@ -0,0 +1,195 @@
+import Dinero from "dinero.js";
+import { User } from "../../../src/models";
+import { isMobile } from "../../support/utils";
+
+type NewTransactionTestCtx = {
+  allUsers?: User[];
+  user?: User;
+  contact?: User;
+};
+
+describe("New Transaction", function () {
+  const ctx: NewTransactionTestCtx = {};
+
+  beforeEach(function () {
+    cy.task("db:seed");
+
+    cy.intercept("GET", "/users*").as("allUsers");
+
+    cy.intercept("GET", "/users/search*").as("usersSearch");
+
+    cy.intercept("POST", "/transactions").as("createTransaction");
+
+    cy.intercept("GET", "/notifications").as("notifications");
+    cy.intercept("GET", "/transactions/public").as("publicTransactions");
+    cy.intercept("GET", "/transactions").as("personalTransactions");
+    cy.intercept("PATCH", "/transactions/*").as("updateTransaction");
+
+    cy.database("filter", "users").then((users: User[]) => {
+      ctx.allUsers = users;
+      ctx.user = users[0];
+      ctx.contact = users[1];
+
+      return cy.loginByXstate(ctx.user.username);
+    });
+  });
+
+  it("navigates to the new transaction form, selects a user and submits a transaction request", function () {
+    const request = {
+      amount: "95",
+      description: "Fancy Hotel 🏨",
+    };
+
+    cy.getBySelLike("new-transaction").click();
+    cy.wait("@allUsers");
+
+    cy.getBySelLike("user-list-item").contains(ctx.contact!.firstName).click({ force: true });
+    cy.visualSnapshot("User Search First Name Input");
+
+    cy.getBySelLike("amount-input").type(request.amount);
+    cy.getBySelLike("description-input").type(request.description);
+    cy.visualSnapshot("Amount and Description Input");
+    cy.getBySelLike("submit-request").click();
+    cy.wait("@createTransaction");
+    cy.getBySel("alert-bar-success")
+      .should("be.visible")
+      .and("have.text", "Transaction Submitted!");
+    cy.visualSnapshot("Transaction Request Submitted Notification");
+
+    cy.getBySelLike("return-to-transactions").click();
+    cy.getBySelLike("personal-tab").click().should("have.class", "Mui-selected");
+
+    cy.getBySelLike("transaction-item").should("contain", request.description);
+    cy.visualSnapshot("Transaction Item Description in List");
+  });
+
+  it("displays new transaction errors", function () {
+    cy.getBySelLike("new-transaction").click();
+    cy.wait("@allUsers");
+
+    cy.getBySelLike("user-list-item").contains(ctx.contact!.firstName).click({ force: true });
+
+    cy.getBySelLike("amount-input").type("43").find("input").clear().blur();
+    cy.get("#transaction-create-amount-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Please enter a valid amount");
+
+    cy.getBySelLike("description-input").type("Fun").find("input").clear().blur();
+    cy.get("#transaction-create-description-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Please enter a note");
+
+    cy.getBySelLike("submit-request").should("be.disabled");
+    cy.getBySelLike("submit-payment").should("be.disabled");
+    cy.visualSnapshot("New Transaction Errors with Submit Payment/Request Buttons Disabled");
+  });
+
+  it("submits a transaction payment and verifies the deposit for the receiver", function () {
+    cy.getBySel("nav-top-new-transaction").click();
+
+    const transactionPayload = {
+      transactionType: "payment",
+      amount: 25,
+      description: "Indian Food",
+      sender: ctx.user,
+      receiver: ctx.contact,
+    };
+
+    // first let's grab the current balance from the UI
+    let startBalance: string;
+    if (!isMobile()) {
+      // only check the balance display in desktop resolution
+      // as it is NOT shown on mobile screen
+      cy.get("[data-test=sidenav-user-balance]")
+        .invoke("text")
+        .then((x) => {
+          startBalance = x; // something like "$1,484.81"
+          expect(startBalance).to.match(/\$\d/);
+        });
+    }
+
+    cy.createTransaction(transactionPayload);
+    cy.wait("@createTransaction");
+    cy.getBySel("new-transaction-create-another-transaction").should("be.visible");
+
+    if (!isMobile()) {
+      // make sure the new balance is displayed
+      cy.get("[data-test=sidenav-user-balance]").should(($el) => {
+        // here we only make sure the text has changed
+        // we could also convert the balance to actual number
+        // and confirm the new balance is the start balance - amount
+        expect($el.text()).to.not.equal(startBalance);
+      });
+    }
+    cy.visualSnapshot("Transaction Payment Submitted Notification");
+
+    cy.switchUserByXstate(ctx.contact!.username);
+
+    const updatedAccountBalance = Dinero({
+      amount: ctx.contact!.balance + transactionPayload.amount * 100,
+    }).toFormat();
+
+    if (isMobile()) {
+      cy.getBySel("sidenav-toggle").click();
+    }
+
+    cy.getBySelLike("user-balance").should("contain", updatedAccountBalance);
+    cy.visualSnapshot("Verify Updated Sender Account Balance");
+  });
+
+  it("submits a transaction request and accepts the request for the receiver", function () {
+    const transactionPayload = {
+      transactionType: "request",
+      amount: 100,
+      description: "Fancy Hotel",
+      sender: ctx.user,
+      receiver: ctx.contact,
+    };
+
+    cy.getBySelLike("new-transaction").click();
+    cy.createTransaction(transactionPayload);
+    cy.wait("@createTransaction");
+    cy.getBySel("new-transaction-create-another-transaction").should("be.visible");
+    cy.visualSnapshot("receiver - Transaction Payment Submitted Notification");
+
+    cy.switchUserByXstate(ctx.contact!.username);
+
+    cy.getBySelLike("personal-tab").click();
+
+    cy.wait("@personalTransactions");
+
+    cy.getBySelLike("transaction-item")
+      .first()
+      .should("contain", transactionPayload.description)
+      .click({ force: true });
+    cy.getBySel("transaction-detail-header").should("exist");
+    cy.visualSnapshot("Navigate to Transaction Item");
+
+    cy.getBySelLike("accept-request").click();
+    cy.wait("@updateTransaction").its("response.statusCode").should("eq", 204);
+    cy.getBySelLike("transaction-detail-header").should("be.visible");
+    cy.getBySelLike("transaction-amount").should("be.visible");
+    cy.getBySelLike("sender-avatar").should("be.visible");
+    cy.getBySelLike("receiver-avatar").should("be.visible");
+    cy.getBySelLike("transaction-description").should("be.visible");
+    cy.visualSnapshot("Accept Transaction Request");
+
+    cy.switchUserByXstate(ctx.user!.username);
+
+    const updatedAccountBalance = Dinero({
+      amount: ctx.user!.balance + transactionPayload.amount * 100,
+    }).toFormat();
+
+    if (isMobile()) {
+      cy.getBySel("sidenav-toggle").click();
+    }
+
+    cy.getBySelLike("user-balance").should("contain", updatedAccountBalance);
+    cy.visualSnapshot("Verify Updated Sender Account Balance");
+  });
+
+  it("navigates to the new transaction form, selects a user and submits a transaction payment", function () {
+    // The following line is meant to fail the test on purpose. You can remove it and update accordingly
+    cy.get("#fail-on-purpose").should("exist");
+  });
+});
---
+++ cypress/tests/ui/user-settings.spec.ts
@@ -0,0 +1,66 @@
+import { User } from "../../../src/models";
+import { isMobile } from "../../support/utils";
+
+describe("User Settings", function () {
+  beforeEach(function () {
+    cy.task("db:seed");
+
+    cy.intercept("PATCH", "/users/*").as("updateUser");
+    cy.intercept("GET", "/notifications*").as("getNotifications");
+
+    cy.database("find", "users").then((user: User) => {
+      cy.loginByXstate(user.username);
+    });
+
+    if (isMobile()) {
+      cy.getBySel("sidenav-toggle").click();
+    }
+
+    cy.getBySel("sidenav-user-settings").click();
+  });
+
+  it("renders the user settings form", function () {
+    cy.wait("@getNotifications");
+    cy.getBySel("user-settings-form").should("be.visible");
+    cy.location("pathname").should("include", "/user/settings");
+
+    cy.visualSnapshot("User Settings Form");
+  });
+
+  it("should display user setting form errors", function () {
+    ["first", "last"].forEach((field) => {
+      cy.getBySelLike(`${field}Name-input`).type("Abc").clear().blur();
+      cy.get(`#user-settings-${field}Name-input-helper-text`)
+        .should("be.visible")
+        .and("contain", `Enter a ${field} name`);
+    });
+
+    cy.getBySelLike("email-input").type("abc").clear().blur();
+    cy.get("#user-settings-email-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Enter an email address");
+
+    cy.getBySelLike("email-input").type("abc@bob.").blur();
+    cy.get("#user-settings-email-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Must contain a valid email address");
+
+    cy.getBySelLike("phoneNumber-input").type("abc").clear().blur();
+    cy.get("#user-settings-phoneNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Enter a phone number");
+
+    cy.getBySelLike("phoneNumber-input").type("615-555-").blur();
+    cy.get("#user-settings-phoneNumber-input-helper-text")
+      .should("be.visible")
+      .and("contain", "Phone number is not valid");
+
+    cy.getBySelLike("submit").should("be.disabled");
+    cy.visualSnapshot("User Settings Form Errors and Submit Disabled");
+  });
+
+  it("updates first name, last name, email and phone number", function () {
+        // The following line is meant to fail the test on purpose. You can remove it and update accordingly
+        cy.get("#fail-on-purpose").should("exist")
+  });
+});
